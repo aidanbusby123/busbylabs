de8d30cf33226412d40e2a731ffa03047a6db443
Aidan Busby <aidanbusby123@gmail.com>
2023-12-07 13:22:59 -0800

    cosmetics of index.php

--- wshsmathclub/problems/index.php
+++ wshsmathclub/problems/index.php
@@ -10,9 +10,9 @@
     <?php
     $files = glob('probs/*.pdf');
     foreach($files as $file){
-    printf('<tr><td><input type="checkbox" name="box[]"></td>
+    printf('<tr>
             <td><a href="%1$s" target="_blank">%1$s</a></td>
-            <td>%2$s</td></tr>', 
+            <td>%2$s</td></tr>\n', 
             basename($file), // or basename($file) for just the filename w\out path
             date('F d Y, H:i:s', filemtime($file)));
 }
