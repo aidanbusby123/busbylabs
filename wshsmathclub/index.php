31142aeaa62c0e9e90ed8907457940418bee8304
Aidan Busby <aidanbusby123@gmail.com>
2023-12-07 13:35:24 -0800

    added php to index

---
+++ wshsmathclub/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>West Seattle High School Math Club</title>
+        <meta name="description" content="The official website of the West Seattle High School Math Club.">
+        <link rel = "stylesheet" href = "../style.css">
+        <link rel="author" content="Aidan Busby">
+        <link rel = "icon" href="../wshsmathclub/media/cowculator.ico">
+    </head>
+    <body>
+        <h1 class="title">West Seattle High School Math Club</h1>
+        <div class="menu">
+            <a href="index.html">Home</a>
+            <a href="about.html">About</a>
+            <a href="https://bit.ly/3RBTxwB" style="border:none"><img src="../wshsmathclub/media/discord-mark-blue.png" width="40vw"></a>
+            <a href="https://instagram.com/wshsmathclub" style="border:none"><img src="../wshsmathclub/media/480px-Instagram_logo_2022.svg.png" width="40vw"></a>
+            <div class="drop-menu">
+                <p class="dropdown">Resources</p>
+                <div class="drop-content-menu">
+                    <ul>
+                        <li><a href="../wshsmathclub/media/(AoPS Competition Preparation) Richard Rusczyk_ Sandor Lehoczky - The Art of Problem Solving, Volume 2_ and Beyond. 2-AoPS Incorporated (2003).pdf">AOPS Volume 2</a></li>
+                        <li><a href="https://seattleschools-my.sharepoint.com/:f:/g/personal/1ambusby_seattleschools_org/Eo-H2bdkCYNMg6jHFmZyb1sBCJVTbVHe-3TBv-RsHeIOsA?e=0v5jJG">PowerPoints</a></li>
+                    </ul>
+                </div>
+            </div>
+            <a href="problems">Problem Sets</a>
+        </div>
+        <br>
+        <?php
+        $files = scandir('data', SCANDIR_SORT_DESCENDING);
+        $newest_file = $files[0];
+        ?>
+    </body>
+</html>
