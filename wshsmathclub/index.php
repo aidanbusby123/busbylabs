9d3ab993061b11fd42b592d3226ceaf71a39df16
Aidan Busby <aidanbusby123@gmail.com>
2023-12-07 19:31:51 -0800

    fixing iframe size

--- wshsmathclub/index.php
+++ wshsmathclub/index.php
@@ -46,7 +46,7 @@
             <a href="%1$s">Download (%2$s)</a>
             <br>
             <div style="height:75vh;">
-            <iframe src="%1$s" width="60%%" height="100%%">
+            <iframe src="%1$s" width="60%%" height="100%%" style="resize:both;">
             </div>
             ',
             $latest_filename, basename($latest_filename));
