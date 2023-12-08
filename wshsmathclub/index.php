4c6a2e26701037608e88542a86b5f154f8feace3
Aidan Busby <aidanbusby123@gmail.com>
2023-12-07 19:38:34 -0800

    fixing iframe size

--- wshsmathclub/index.php
+++ wshsmathclub/index.php
@@ -41,12 +41,14 @@
                     }
             }
             printf('
+            <center>
             <h2>Current Problem Set</h2>
             <a href="%1$s">Download (%2$s)</a>
             <br>
             <div style="height:70vh;">
             <iframe src="%1$s" width="50%%" height="100%%" allowfullscreen>
             </div>
+            </center>
             ',
             $latest_filename, basename($latest_filename));
             ?>
